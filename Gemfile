83eed3d918defc64fdc0864654c4c84e51b9cd4a
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2016-06-23 00:09:16 +0300

    Adding rspec.

--- Gemfile
+++ Gemfile
@@ -5,3 +5,7 @@ source "https://rubygems.org"
 gem 'rake'
 gem 'rest-client'
 gem 'mail'
+
+group :development, :test do
+    gem 'rspec'
+end
---
+++ spec/access_token_repository_spec.rb
@@ -0,0 +1,7 @@
+require 'app/access_token_repository'
+
+describe AccessTokenRepository do
+  it 'does something' do
+    expect(subject).not_to eq object
+  end
+end
