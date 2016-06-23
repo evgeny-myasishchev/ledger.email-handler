8a0823665016d78b1f9919d881f2acbd9e5dce08
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2016-06-24 00:44:30 +0300

    Implementing access token repo.

--- app/access_token_repository.rb
+++ app/access_token_repository.rb
@@ -1,11 +1,15 @@
+require 'json'
+
 class AccessTokenRepository
   def initialize(data_dir)
-    # test
+    @access_tokens_dir = data_dir.join('access-tokens')
   end
 
   def save(email, access_token)
+    @access_tokens_dir.join(email).write(JSON.pretty_generate(access_token))
   end
 
   def load(email)
+    JSON.parse @access_tokens_dir.join(email).read
   end
 end
--- spec/access_token_repository_spec.rb
+++ spec/access_token_repository_spec.rb
@@ -1,7 +1,29 @@
 require 'app/access_token_repository'
+require 'fileutils'
 
 describe AccessTokenRepository do
-  it 'does something' do
-    expect(subject).not_to eq object
+  let(:data_dir) { Pathname.new(File.expand_path('../tmp/access-token-repo', __dir__)) }
+  let(:access_tokens_dir) { data_dir.join('access-tokens') }
+  subject { described_class.new(data_dir) }
+
+  before(:each) do
+    FileUtils.rm_rf access_tokens_dir if access_tokens_dir.exist?
+    FileUtils.mkdir_p access_tokens_dir
+  end
+
+  describe 'save' do
+    let(:email) { FFaker::Internet.email }
+    let(:token) { fake_access_token }
+
+    it 'should save the access token to a data_dir/access-tokens' do
+      subject.save email, token
+      expect(access_tokens_dir.join(email)).to exist
+      expect(JSON.parse(access_tokens_dir.join(email).read)).to eql token
+    end
+
+    it 'should read the token from file' do
+      access_tokens_dir.join(email).write(JSON.generate(token))
+      expect(subject.load(email)).to eql(token)
+    end
   end
 end
