1bcbe67427913347d5447ac72ae35023152b5f5f
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2016-07-20 00:25:26 +0300

    Implementing logic to create new api instance.

--- app/ledger_api.rb
+++ app/ledger_api.rb
@@ -1,10 +1,15 @@
 require 'app/lib/request'
 
 class LedgerApi
+  CSRF_TOKEN_NAME = 'form_authenticity_token'.freeze
+  SESSION_COOKIE_NAME = '_ledger_session_v1'.freeze
   Log = Logger.get(self)
 
-  def initialize(session, _csrf_token)
+  attr_reader :session, :csrf_token
+
+  def initialize(session, csrf_token)
     @session = session
+    @csrf_token = csrf_token
   end
 
   def report_pending_transaction(pending_transaction)
@@ -22,7 +27,8 @@ class LedgerApi
     sessions_url = "#{Settings.ledger.api_host}/api/sessions"
     puts "sessions_url: #{sessions_url}"
     Log.debug "Posting google_id_token onto #{sessions_url}"
-    session_data = Request.post sessions_url, params
-    new(nil, session_data['form_authenticity_token'])
+    response = Request.post sessions_url, params
+    response_data = JSON.parse response.body
+    new(response.cookies[SESSION_COOKIE_NAME], response_data[CSRF_TOKEN_NAME])
   end
 end
--- spec/ledger_api_spec.rb
+++ spec/ledger_api_spec.rb
@@ -10,25 +10,27 @@ describe LedgerApi do
     before(:each) do
       allow(LedgerApi).to receive(:new).and_call_original
       stub_request(:post, "#{api_host}/api/sessions")
-        .with(body: {
-                google_id_token: google_id_token
-              })
-        .to_return({
-                     body: {
-                       form_authenticity_token: form_authenticity_token
-                     }.to_json
-                   },
-                   headers: {
-                     'Set-Cookie' => ledger_session_cookie
-                   })
+        .with(body: { google_id_token: google_id_token })
+        .to_return(
+          body: { form_authenticity_token: form_authenticity_token }.to_json,
+          headers: { 'Set-Cookie' => "_ledger_session_v1=#{ledger_session_cookie}" }
+        )
     end
 
     it 'should initiate new session and create new api instance with the sesion cookie and the result' do
       api = LedgerApi.create(google_id_token)
       expect(api).to be_a(LedgerApi)
+      expect(api.csrf_token).to eql(form_authenticity_token)
+      expect(api.session).to eql(ledger_session_cookie)
     end
 
     it 'should raise error if sessions api returns non 200 status code' do
+      stub_request(:post, "#{api_host}/api/sessions")
+        .with(body: { google_id_token: google_id_token })
+        .to_return(status: 500)
+      expect do
+        LedgerApi.create(google_id_token)
+      end.to raise_error RestClient::InternalServerError
     end
   end
 end
