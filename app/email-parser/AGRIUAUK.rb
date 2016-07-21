1aed0cd764fccee76d8f4794cf1593ed80bdf0a4
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2016-07-22 00:57:48 +0300

    Implementing basic parsing logic.

--- app/email-parser/AGRIUAUK.rb
+++ app/email-parser/AGRIUAUK.rb
@@ -1,6 +1,49 @@
 class EmailParser
   class AgricoleParser
-    def self.parse_email(_mail)
+    AMOUNT_REGEX = /^(?<amount>\d+(\.\d+)?)(?<currency>\w{3})(?<status>.*)$/
+    class << self
+      def parse_email(mail)
+        body = mail.body.to_s
+        offset = body.index('!uvedomlenie!')
+        date, offset = next_data_value(offset, 'Data:', body) do |value|
+          DateTime.strptime value, '%d/%m %H:%M'
+        end
+        bank_account, offset = next_data_value(offset, 'Karta:*', body)
+        amount_data, offset = next_data_value(offset, 'Summa=', body) do |value|
+          match_data = value.match AMOUNT_REGEX
+          {
+            amount: match_data[:amount],
+            currency: match_data[:currency],
+            status: match_data[:status]
+          }
+        end
+        balance_data, offset = next_data_value(offset, 'Balans=', body) do |value|
+          match_data = value.match AMOUNT_REGEX
+          {
+            amount: match_data[:amount],
+            currency: match_data[:currency]
+          }
+        end
+        comment, offset = next_data_value(offset, 'Mesto:', body)
+        raw_transaction = {
+          type: PendingTransaction::EXPENSE_TYPE_ID,
+          date: date,
+          bank_account: bank_account,
+          amount: amount_data[:amount],
+          comment: comment
+        }
+        raw_transaction
+      end
+
+      private
+
+      def next_data_value(start_at, token, body)
+        token_start = body.index(token, start_at)
+        line_end = body.index("\n", token_start)
+        raw_value = body[token_start + token.length..line_end].strip
+        value = block_given? ? yield(raw_value) : raw_value
+        [value, line_end]
+      end
     end
   end
 end
--- spec/email-parser/agriuauk_spec.rb
+++ spec/email-parser/agriuauk_spec.rb
@@ -1,11 +1,53 @@
-describe 'email-parser' do
-  describe 'AGRIUAUK' do
-    describe 'parse_email' do
-      xit 'should parse expense email' do
-      end
+require 'mail'
+require 'app/email-parser/AGRIUAUK'
 
-      xit 'should parse income email' do
-      end
+describe EmailParser::AgricoleParser do
+  subject { described_class }
+  describe 'parse_email' do
+    it 'should parse expense email' do
+      raw_body = <<RAW_MAIL_BODY
+Date: Mon, 20 Jun 2016 09:45:40 +0300 (EEST)
+From: gsm@credit-agricole.com.ua
+To: 380675461301@sms.upc.smpp
+Cc: evgmya@gmail.com
+Message-ID: <465681514.3559243.1466405140761.JavaMail.apache@pusher.upc.intranet>
+Subject:
+
+!uvedomlenie!
+Data:20/06 09:45
+Karta:*4164
+Summa= 338UAH(Uspeshno)
+Balans= 19899.79UAH
+Mesto:PortoR221(Porto_R22)
+RAW_MAIL_BODY
+      mail = Mail.new raw_body
+      raw_transaction = subject.parse_email mail
+      expect(raw_transaction[:type]).to eql PendingTransaction::EXPENSE_TYPE_ID
+      expect(raw_transaction[:date]).to eql DateTime.iso8601('2016-06-20T09:45')
+      expect(raw_transaction[:bank_account]).to eql '4164'
+      expect(raw_transaction[:amount]).to eql '338'
+      expect(raw_transaction[:comment]).to eql 'PortoR221(Porto_R22)'
+    end
+
+    xit 'should parse income email' do
+    end
+
+    xit 'should handle decimal amount' do
+    end
+
+    xit 'should fail if start pattern not found' do
+    end
+
+    xit 'should fail if expected attributes not found' do
+    end
+
+    xit 'should fail Summa has unexpected format' do
+    end
+
+    xit 'should fail Summa Balans has unexpected format' do
+    end
+
+    xit 'should include currency in the comment if it was different from balans' do
     end
   end
 end
