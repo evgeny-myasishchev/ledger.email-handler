52e0db16cd6c1722b331da52a18e53943230fe20
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2016-07-21 08:50:21 +0300

    Adding pending specs.

---
+++ spec/email-parser/agriuauk_spec.rb
@@ -0,0 +1,11 @@
+describe 'email-parser' do
+  describe 'AGRIUAUK' do
+    describe 'parse_email' do
+      xit 'should parse expense email' do
+      end
+
+      xit 'should parse income email' do
+      end
+    end
+  end
+end
