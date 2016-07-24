8c59ba14741d48130631014baaedb7fc2b78b3e2
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2016-07-24 10:14:51 +0300

    Some cleanup.

--- app/email-parser/AGRIUAUK.rb
+++ app/email-parser/AGRIUAUK.rb
@@ -4,46 +4,22 @@ module EmailParser
     class << self
       def parse_email(mail)
         body = mail.body.to_s
-        offset = body.index('!uvedomlenie!')
-        raise EmailParser::ParserError, 'Start pattern not found' unless offset
-        date, offset = next_data_value(offset, 'Data:', body) do |value|
-          DateTime.strptime value, '%d/%m %H:%M'
-        end
+        offset = detect_start body
+        date, offset = next_data_value(offset, 'Data:', body) { |value| DateTime.strptime value, '%d/%m %H:%M' }
         bank_account, offset = next_data_value(offset, 'Karta:*', body)
-        amount_data, offset = next_data_value(offset, 'Summa=', body) do |value|
-          match_data = value.match AMOUNT_REGEX
-          raise EmailParser::ParserError, 'Summa value has unexpected format' unless match_data
-          {
-            amount: match_data[:amount],
-            currency: match_data[:currency],
-            status: match_data[:status],
-            type: match_data[:type]
-          }
-        end
-        balance_data, offset = next_data_value(offset, 'Balans=', body) do |value|
-          match_data = value.match AMOUNT_REGEX
-          raise EmailParser::ParserError, 'Balans value has unexpected format' unless match_data
-          {
-            amount: match_data[:amount],
-            currency: match_data[:currency],
-            value: match_data
-          }
-        end
+        amount_data, offset = next_data_value(offset, 'Summa=', body, &method(:parse_summa))
+        balance_data, offset = next_data_value(offset, 'Balans=', body, &method(:parse_balans))
         comment, _offset = next_data_value(offset, 'Mesto:', body, line_end: body.length)
-        if amount_data[:currency] != balance_data[:currency]
-          comment = "Amount is #{amount_data[:currency]}. Balance #{balance_data[:value]}. #{comment}"
-        end
-        raw_transaction = {
-          type: amount_data[:type] == '-' ? PendingTransaction::INCOME_TYPE_ID : PendingTransaction::EXPENSE_TYPE_ID,
-          date: date,
-          bank_account: bank_account,
-          amount: amount_data[:amount],
-          comment: comment
-        }
-        raw_transaction
+        build_raw_transaction amount_data, balance_data, date, bank_account, comment
       end
 
       private
+
+      def detect_start(body)
+        offset = body.index('!uvedomlenie!')
+        raise EmailParser::ParserError, 'Start pattern not found' unless offset
+        offset
+      end
 
       def next_data_value(start_at, token, body, line_end: nil)
         token_start = body.index(token, start_at)
@@ -52,6 +28,43 @@ module EmailParser
         raw_value = body[token_start + token.length..line_end].strip
         value = block_given? ? yield(raw_value) : raw_value
         [value, line_end]
+      end
+
+      def parse_summa(value)
+        match_data = value.match AMOUNT_REGEX
+        raise EmailParser::ParserError, 'Summa value has unexpected format' unless match_data
+
+        {
+          amount: match_data[:amount],
+          currency: match_data[:currency],
+          status: match_data[:status],
+          type: match_data[:type]
+        }
+      end
+
+      def parse_balans(value)
+        match_data = value.match AMOUNT_REGEX
+
+        raise EmailParser::ParserError, 'Balans value has unexpected format' unless match_data
+        {
+          amount: match_data[:amount],
+          currency: match_data[:currency],
+          value: match_data
+        }
+      end
+
+      def build_raw_transaction(amount_data, balance_data, date, bank_account, comment)
+        if amount_data[:currency] != balance_data[:currency]
+          comment = "Amount is #{amount_data[:currency]}. Balance #{balance_data[:value]}. #{comment}"
+        end
+
+        {
+          type: amount_data[:type] == '-' ? PendingTransaction::INCOME_TYPE_ID : PendingTransaction::EXPENSE_TYPE_ID,
+          date: date,
+          bank_account: bank_account,
+          amount: amount_data[:amount],
+          comment: comment
+        }
       end
     end
   end
