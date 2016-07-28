81514fbc00190220980757b0ce73a018fd55bfef
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2016-07-28 09:04:17 +0300

    Registering email config service.

--- app/lib/services.rb
+++ app/lib/services.rb
@@ -1,11 +1,14 @@
 require 'app/access_token_repository'
 require 'app/accounts_mapping_config'
+require 'app/email_config'
 
 class Services
   attr_reader :access_token_repo,
-              :accounts_mapping_config
+              :accounts_mapping_config,
+              :email_config
   def initialize(data_dir)
     @access_token_repo = AccessTokenRepository.new data_dir
     @accounts_mapping_config = AccountsMappingConfig.new data_dir
+    @email_config = EmailConfig.new data_dir
   end
 end
