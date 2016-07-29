29dddbc0b4fc46bb347623b669f453570cac58e8
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2016-07-29 09:08:06 +0300

    Refactoring email config. Introducing FS based version and shared specs.

--- app/email_config.rb
+++ app/email_config.rb
@@ -1,9 +1,4 @@
 class EmailConfig
-  Log = Logger.get self
-  def initialize(data_dir)
-    @config_dir = data_dir.join('email-config')
-  end
-
   # Email provider settings example
   # {
   #   'AGRIUAUK' => {
@@ -21,26 +16,10 @@ class EmailConfig
   # bic - BIC to fetch email
   # settings - Emails provider settings to get emails (see format above)
   def add_email_settings(user_email, bic, emails_provider_settings)
-    Log.debug "Adding email settings for user: #{user_email}, bic: #{bic}"
-    @config_dir.mkdir unless @config_dir.exist?
-    config_file = @config_dir.join(user_email)
-    settings = nil
-    if config_file.exist?
-      Log.debug 'Updating existing settings'
-      settings = JSON.parse config_file.read
-    else
-      Log.debug 'Initializing new settings'
-      settings = {}
-    end
-    settings[bic] = emails_provider_settings
-    config_file.write JSON.generate settings
   end
 
   # Returns emails provider settings for given ledger user_email (see format above)
   def get_email_settings(user_email)
-    config_file = @config_dir.join(user_email)
-    raise "Email settings for user '#{user_email}' not found" unless config_file.exist?
-    JSON.parse config_file.read
   end
 
   # Returns an array of results returned by get_email_settings keyed by user_email:
@@ -48,8 +27,43 @@ class EmailConfig
   #   { 'mail@domain.com' => { ...object returned by get_email_settings... } }
   # ]
   def all_email_settings
-    @config_dir.children(false).map do |path|
-      { path.basename.to_s => get_email_settings(path.basename) }
+  end
+
+  # File based email config
+  class FS
+    Log = Logger.get self
+
+    def initialize(data_dir)
+      @config_dir = data_dir.join('email-config')
+    end
+
+    def add_email_settings(user_email, bic, emails_provider_settings)
+      Log.debug "Adding email settings for user: #{user_email}, bic: #{bic}"
+      @config_dir.mkdir unless @config_dir.exist?
+      config_file = @config_dir.join(user_email)
+      settings = nil
+      if config_file.exist?
+        Log.debug 'Updating existing settings'
+        settings = JSON.parse config_file.read
+      else
+        Log.debug 'Initializing new settings'
+        settings = {}
+      end
+      settings[bic] = emails_provider_settings
+      config_file.write JSON.generate settings
+    end
+
+    # Returns emails provider settings for given ledger user_email (see format above)
+    def get_email_settings(user_email)
+      config_file = @config_dir.join(user_email)
+      raise "Email settings for user '#{user_email}' not found" unless config_file.exist?
+      JSON.parse config_file.read
+    end
+
+    def all_email_settings
+      @config_dir.children(false).map do |path|
+        { path.basename.to_s => get_email_settings(path.basename) }
+      end
     end
   end
 end
--- spec/email_config_spec.rb
+++ spec/email_config_spec.rb
@@ -1,9 +1,6 @@
 require 'app/email_config'
 
 describe EmailConfig do
-  let(:data_dir) { Pathname.new(File.expand_path('../tmp/email_config_data_dir', __dir__)) }
-  let(:email_config_dir) { data_dir.join('email-config') }
-
   let(:email1) { fake_email('email1') }
   let(:email2) { fake_email('email2') }
   let(:bic1) { fake_string('BIC1') }
@@ -12,57 +9,82 @@ describe EmailConfig do
   let(:provider_settings1) { { 'option1' => fake_string('value1'), 'option2' => fake_string('value2') } }
   let(:provider_settings2) { { 'option1' => fake_string('value1'), 'option2' => fake_string('value2') } }
 
-  subject { described_class.new(data_dir) }
+  shared_examples 'standard email config' do
+    before(:each) do
+      subject.add_email_settings email1, bic1, provider_settings1
+    end
 
-  before(:each) do
-    FileUtils.rm_rf data_dir if data_dir.exist?
-    FileUtils.mkdir_p data_dir
-    subject.add_email_settings email1, bic1, provider_settings1
+    describe 'add_email_settings' do
+      it 'should create new settings with user settings for given bic' do
+        config_file = email_config_dir.join(email1)
+        expect(config_file).to exist
+        data = subject.get_email_settings email1
+        expect(data).to eql(bic1 => provider_settings1)
+      end
+
+      it 'should add new bic section to existing settings' do
+        subject.add_email_settings email1, bic2, provider_settings2
+        data = subject.get_email_settings email1
+        expect(data).to eql(bic1 => provider_settings1,
+                            bic2 => provider_settings2)
+      end
+
+      it 'should update existing section of bic settings' do
+        subject.add_email_settings email1, bic1, provider_settings2
+        data = subject.get_email_settings email1
+        expect(data).to eql(bic1 => provider_settings2)
+      end
+    end
+
+    describe 'get_email_settings' do
+      it 'should return settings of given user' do
+        expect(subject.get_email_settings(email1)).to eql(bic1 => provider_settings1)
+      end
+
+      it 'should raise error if settings not found' do
+        expect { subject.get_email_settings(email2) }.to raise_error "Email settings for user '#{email2}' not found"
+      end
+    end
+
+    describe 'all_email_settings' do
+      it 'should return settings of all users' do
+        subject.add_email_settings email1, bic2, provider_settings2
+        subject.add_email_settings email2, bic2, provider_settings2
+        expect(subject.all_email_settings).to eql [
+          { email1 => {
+            bic1 => provider_settings1,
+            bic2 => provider_settings2
+          } },
+          { email2 => { bic2 => provider_settings2 } }
+        ]
+      end
+    end
   end
 
-  describe 'add_email_settings' do
-    it 'should create new settings with user settings for given bic' do
-      config_file = email_config_dir.join(email1)
-      expect(config_file).to exist
-      data = subject.get_email_settings email1
-      expect(data).to eql(bic1 => provider_settings1)
+  describe 'FS' do
+    let(:data_dir) { Pathname.new(File.expand_path('../tmp/email_config_data_dir', __dir__)) }
+    let(:email_config_dir) { data_dir.join('email-config') }
+
+    it_behaves_like 'standard email config'
+
+    subject { described_class::FS.new(data_dir) }
+
+    before(:each) do
+      FileUtils.rm_rf data_dir if data_dir.exist?
+      FileUtils.mkdir_p data_dir
     end
 
-    it 'should add new bic section to existing settings' do
-      subject.add_email_settings email1, bic2, provider_settings2
-      data = subject.get_email_settings email1
-      expect(data).to eql(bic1 => provider_settings1,
-                          bic2 => provider_settings2)
-    end
-
-    it 'should update existing section of bic settings' do
-      subject.add_email_settings email1, bic1, provider_settings2
-      data = subject.get_email_settings email1
-      expect(data).to eql(bic1 => provider_settings2)
-    end
-  end
-
-  describe 'get_email_settings' do
-    it 'should return settings of given user' do
-      expect(subject.get_email_settings(email1)).to eql(bic1 => provider_settings1)
-    end
-
-    it 'should raise error if settings not found' do
-      expect { subject.get_email_settings(email2) }.to raise_error "Email settings for user '#{email2}' not found"
-    end
-  end
-
-  describe 'all_email_settings' do
-    it 'should return settings of all users' do
-      subject.add_email_settings email1, bic2, provider_settings2
+    it 'should store data on file system' do
+      subject.add_email_settings email1, bic1, provider_settings1
       subject.add_email_settings email2, bic2, provider_settings2
-      expect(subject.all_email_settings).to eql [
-        { email1 => {
-          bic1 => provider_settings1,
-          bic2 => provider_settings2
-        } },
-        { email2 => { bic2 => provider_settings2 } }
-      ]
+
+      email1_file = email_config_dir.join(email1)
+      expect(email1_file).to exist
+      expect(JSON.parse(email1_file.read)).to eql bic1 => provider_settings1
+
+      email2_file = email_config_dir.join(email2)
+      expect(email2_file).to exist
+      expect(JSON.parse(email2_file.read)).to eql bic2 => provider_settings2
     end
   end
 end
