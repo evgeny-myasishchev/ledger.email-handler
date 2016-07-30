c2c02e32995d96b4c28ba06ace43949692701a9c
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2016-07-30 09:24:36 +0300

    Data prep

--- spec/email_handler_service_spec.rb
+++ spec/email_handler_service_spec.rb
@@ -4,7 +4,10 @@ require 'app/email_handler_service'
 
 describe EmailHandlerService do
   let(:data_dir) { Pathname.new(File.expand_path('../tmp/email-handler-service-spec-data', __dir__)) }
-  let(:services) { Services.new data_dir }
+  let(:email_config) { EmailConfig::InMemory.new }
+  let(:services) do
+    Services.new(data_dir) { |svc| svc[:email_config] = email_config }
+  end
 
   let(:email1) { fake_email('email1') }
   let(:email2) { fake_email('email2') }
@@ -22,12 +25,18 @@ describe EmailHandlerService do
   let(:provider_settings12) { { 'in-memory' => [mail121, mail122] } }
   let(:provider_settings21) { { 'in-memory' => [mail211, mail212] } }
 
+  let(:email1_access_token) { fake_access_token(id_token_email: email1) }
+  let(:email2_access_token) { fake_access_token(id_token_email: email2) }
+
   before(:each) do
     FileUtils.rm_rf data_dir if data_dir.exist?
     FileUtils.mkdir_p data_dir
-    services.email_config.add_email_settings email1, bic1, provider_settings11
-    services.email_config.add_email_settings email1, bic2, provider_settings12
-    services.email_config.add_email_settings email2, bic1, provider_settings21
+    email_config.add_email_settings email1, bic1, provider_settings11
+    email_config.add_email_settings email1, bic2, provider_settings12
+    email_config.add_email_settings email2, bic1, provider_settings21
+
+    services.access_token_repo.save email1, email1_access_token
+    services.access_token_repo.save email2, email2_access_token
   end
 
   describe 'handle_emails' do
