d10138526a88c8bc419bebf1af3fab4f3ba6c711
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2016-09-21 08:31:54 +0300

    Adding error handling. Adding different currency matching.

--- app/email_parser/PBANUA2X.rb
+++ app/email_parser/PBANUA2X.rb
@@ -1,9 +1,14 @@
 module EmailParser
   class PBANUA2X < BaseParser
     class << self
+      INCOME_PATTERN = /^Поповнення$/
+      BASE_AMOUNT_PATTERN = '(?<value>\\d+\.\\d{2})(?<currency>\\w{3})'.freeze
+      AMOUNT_PATTERN = Regexp.compile("^#{BASE_AMOUNT_PATTERN}")
+      BALANCE_PATTERN = Regexp.compile("^Бал. #{BASE_AMOUNT_PATTERN}")
+
       def parse_email(mail)
         body = extract_body mail
-        amount_match = body.match(/^(?<value>\d+\.\d{2})\w{3}/)
+        amount_match = body.match(AMOUNT_PATTERN)
         raise_parser_error(mail, 'Leading amount not found') unless amount_match
 
         bank_account_match = amount_match.post_match.match(/\d\*\d{2}/)
@@ -16,16 +21,26 @@ module EmailParser
 
         date = DateTime.iso8601 mail.date.strftime("%FT#{time_match.to_s.strip}:00%:z")
 
-        build_raw_transaction amount_match[:value], date, bank_account_match.to_s, comment.to_s
+        # if balance currency is different then payment was in different currency
+        balance_match = time_match.post_match.lstrip.match(BALANCE_PATTERN)
+        raise_parser_error(mail, 'Balance not found') unless balance_match
+        # if balance_match[:currency] != amount_match[:currency]
+        #   puts '==hello'
+        # end
+
+        type_id = PendingTransaction::EXPENSE_TYPE_ID
+        type_id = PendingTransaction::INCOME_TYPE_ID if comment.index(INCOME_PATTERN)
+
+        build_raw_transaction amount_match[:value], date, bank_account_match.to_s, comment.to_s, type_id
       end
 
-      def build_raw_transaction(amount, date, bank_account, comment)
+      def build_raw_transaction(amount, date, bank_account, comment, type_id)
         {
           amount: amount,
           date: date,
           comment: comment,
           bank_account: bank_account,
-          type_id: PendingTransaction::EXPENSE_TYPE_ID
+          type_id: type_id
         }
       end
 
--- spec/email-parser/pbanua2x_spec.rb
+++ spec/email-parser/pbanua2x_spec.rb
@@ -9,6 +9,10 @@ describe EmailParser::PBANUA2X do
     "#{SecureRandom.random_number(10)}*#{SecureRandom.random_number(100).to_s.rjust(2, '0')}"
   end
 
+  def new_mail(&block)
+    Mail.new('Message-ID' => fake_string('MSG-ID'), &block)
+  end
+
   describe 'parse_email' do
     it 'should parse expense transaction' do
       date = DateTime.parse FFaker::Time.datetime(hours: 23, minuts: 32)
@@ -18,7 +22,7 @@ describe EmailParser::PBANUA2X do
       description = 'Аптека Аптека Класc'
       account = fake_bank_account
       time = date.strftime('%H:%M')
-      mail = Mail.new do
+      mail = new_mail do
         date date.iso8601
         body %(
 #{amount}#{currency} #{description} #{account} #{time} Бал. 1824.88UAH Бал. Бонус+ 135.40UAH
@@ -31,6 +35,31 @@ Lorem ipsum dolor sit amet
       expect(raw_transaction[:bank_account]).to eql account
       expect(raw_transaction[:amount]).to eql amount
       expect(raw_transaction[:comment]).to eql description
+    end
+
+    it 'should match some comment patterns as income' do
+      mail = new_mail do
+        date DateTime.now
+        body %(25326.92UAH Поповнення 5*38 11:26 Бал. 25328.95UAH)
+      end
+      raw_transaction = subject.parse_email mail
+      expect(raw_transaction[:type_id]).to eql PendingTransaction::INCOME_TYPE_ID
+    end
+
+    it 'should match different currency and extract calculate actual amount' do
+      currency = fake_currency
+      amount = fake_amount
+
+      balance_currency = fake_currency
+      balance_amount = fake_amount
+
+      mail = new_mail do
+        date DateTime.now
+        body %(#{amount}#{currency} Iнтернет-магазин LONDON 2*43 10:03 Бал. #{balance_amount}#{balance_currency} Курс 26.6666 UAH/USD Кред.лiм 1UAH)
+      end
+      raw_transaction = subject.parse_email mail
+      expect(raw_transaction[:amount]).to eql((amount.to_f * 26.6666).round(2))
+      expect(raw_transaction[:comment]).to eql("Iнтернет-магазин LONDON. Actual amount #{amount}#{currency} Курс 26.6666")
     end
 
     it 'should raise error if has no leading amount' do
@@ -64,6 +93,18 @@ Lorem ipsum dolor sit amet
       end
 
       expect { subject.parse_email(mail) }.to raise_error EmailParser::ParserError, "Time not found. MailId: #{mail['Message-ID']}"
+    end
+
+    it 'should raise error if balance not found' do
+      mail = Mail.new('Message-ID' => fake_string('MSG-ID')) do
+        date DateTime.now
+        body %(
+294.00UAH Ресторан Феличе 3*43 22:56 xxxx Бал. Бонус+ 135.40UAH
+Lorem ipsum dolor sit amet
+)
+      end
+
+      expect { subject.parse_email(mail) }.to raise_error EmailParser::ParserError, "Balance not found. MailId: #{mail['Message-ID']}"
     end
   end
 end
